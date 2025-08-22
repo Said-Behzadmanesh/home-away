87668134ec216fc563329ebdff1b56973d0b8f8a
Said Behzadmanesh <behmanesh_s@yahoo.com>
2025-08-22 10:23:23 +0200

    introducing dark, light and system mode

--- components/navbar/LinksDropdown.tsx
+++ components/navbar/LinksDropdown.tsx
@@ -1,5 +1,43 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+  DropdownMenuSeparator,
+} from "@/components/ui/dropdown-menu";
+import { LuAlignLeft } from "react-icons/lu";
+import Link from "next/link";
+import { Button } from "../ui/button";
+import UserIcon from "./UserIcon";
+import { links } from "@/utils/links";
+import SignOutLink from "./SignOutLink";
+
 function LinksDropdown() {
-  return <div>LinksDropdown</div>;
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" className="flex gap-4 max-w-[100px]">
+          <LuAlignLeft className="w-6 h-6" />
+          <UserIcon />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="start" className="w-52" sideOffset={10}>
+        {links.map((link) => {
+          return (
+            <DropdownMenuItem key={link.href}>
+              <Link href={link.href} className="capitalize w-full">
+                {link.label}
+              </Link>
+            </DropdownMenuItem>
+          );
+        })}
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <SignOutLink />
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 }
 
 export default LinksDropdown;
